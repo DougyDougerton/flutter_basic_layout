73cf3d556f94e511e0e2632f5750e3f6c4bb7832
Douglas Spicer <doug@Douglass-MacBook-Air.local>
2024-08-12 15:38:51 -0600

    Updating a file

---
+++ lib/main_three.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.yellow,
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text('Cris'),
+            Text('Doug'),
+            Text('Carson'),
+            Text('Pablito'),
+          ],
+        ),
+      ), 
+      drawer: Drawer(
+      child: ListView(
+         children: const [
+          DrawerHeader(
+            child: Text('Header'),
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 0, 255, 8),
+            ),
+          ),
+          ListTile(
+            title: Text('Stuff'),
+            onTap: null,
+          ),
+            ListTile(
+            title: Text('and'),
+            onTap: null,
+          ),
+          ListTile(
+            title: Text('Things'),
+            onTap: null,
+          ),
+         ],
+
+      )),
+      bottomNavigationBar: const BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              iconSize: 30,
+              onPressed: null, 
+              icon: Icon(Icons.home)),
+            IconButton(
+              iconSize: 30,
+              onPressed: null, 
+              icon: Icon(Icons.person)),
+            IconButton(
+              iconSize: 30,
+              onPressed: null, 
+              icon: Icon(Icons.settings))
+          ],
+        ),
+      ),
+    );
+  }
+}
